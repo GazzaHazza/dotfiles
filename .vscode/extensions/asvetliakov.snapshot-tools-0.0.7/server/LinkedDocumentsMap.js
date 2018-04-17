520b67338fd0be6f1184cad38aea0b3280f8abbe
Gary Hanks <gary.hanks@housesimple.com>
2018-04-17 12:12:11 +0100

    Added vsCode settings

--- .vscode/extensions/asvetliakov.snapshot-tools-0.0.7/server/LinkedDocumentsMap.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class LinkedDocumentsMap extends Map {
-}
-exports.LinkedDocumentsMap = LinkedDocumentsMap;
-//# sourceMappingURL=LinkedDocumentsMap.js.map
--- .vscode/extensions/kevinkyang.auto-comment-blocks-1.0.1/out/src/onEnterRules.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=onEnterRules.js.map
